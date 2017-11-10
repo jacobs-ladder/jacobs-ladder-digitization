ddba0b3b7615962d1b126e12c0d81244cddff8d2
Ryan Voor <ryan.voor@gmail.com>
2017-11-10 11:05:55 -0500

    added skeleton for python's db lib

---
+++ db_lib.py
@@ -0,0 +1,11 @@
+from login import *
+import psycopg2
+
+#https://www.tutorialspoint.com/postgresql/postgresql_python.htm
+
+#user_name, user_password
+def authenticate():
+    pass # TODO temp
+
+def get_user():
+    pass # TODO temp
