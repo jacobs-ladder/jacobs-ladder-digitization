eff8b6fb10ffce1c21bab90a1ad96cf7b68cf5de
Nathan Weiskirch <weiskirchn@gmail.com>
2017-11-13 10:35:32 -0500

    Added some placeholders to db_lib

--- db_lib.py
+++ db_lib.py
@@ -1,11 +1,13 @@
-from login import *
 import psycopg2
 
 #https://www.tutorialspoint.com/postgresql/postgresql_python.htm
 
 #user_name, user_password
-def authenticate():
+def authenticate(username, password):
+    return True # TODO temp
+
+def get_user(user_id):
     pass # TODO temp
 
-def get_user():
-    pass # TODO temp
+def get_user_id(username):
+    return username.split('user')[1] # TODO temp
