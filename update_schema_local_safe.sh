345ea8c39a69bef77ce9ce5e8729882b3b6c1f49
Ryan Voor <ryan.voor@gmail.com>
2017-11-14 12:22:23 -0500

    fixed db update script bug and specified it for local db usage

--- update_schema_local_safe.sh
+++ update_schema_local_safe.sh
@@ -4,16 +4,14 @@
 # AND IF THERE IS AN ERROR WHILE APPLYING ALTERS THEN THERE WILL BE
 # A FILE CALLED database_alters_output.txt WITH THE ERROR CODE OUTPUT
 
-# TODO the way that this script updates the database should probably be
-# changed once we have the database hosted somewhere and we aren't running
-# 6 separate local instances
+# This version of the script only works on the local database and it doesnt change the database unless all the alters apply correctly
 
 # create temp database
 
 database_user="postgres"
 temp_database_name="jacobs_ladder_digitization_temp"
 
-psql -U $database_user -1 -c "CREATE DATABASE $temp_database_name" &> /dev/null
+psql -U $database_user -c "CREATE DATABASE $temp_database_name" &> /dev/null
 
 # fill temp database with new alters
 
@@ -30,6 +28,9 @@ while read line; do
 done < $order_of_alters_full_file_name
 
 # check for errors when applying the alters
+
+# TODO testing
+cat $temp_database_alters_output_full_file_name
 
 if grep -q ERROR $temp_database_alters_output_full_file_name
 then
@@ -48,11 +49,11 @@ else
 
     # drop original database
 
-    psql -U $database_user -1 -c "DROP DATABASE $original_database_name" &> /dev/null
+    psql -U $database_user -c "DROP DATABASE $original_database_name" &> /dev/null
 
     # rename temp to original name
 
-    psql -U $database_user -1 -c "ALTER DATABASE $temp_database_name RENAME TO $original_database_name" &> /dev/null
+    psql -U $database_user -c "ALTER DATABASE $temp_database_name RENAME TO $original_database_name" &> /dev/null
 
     # repopulate data
     ./repopulate_data.sh
