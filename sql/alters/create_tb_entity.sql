eeb79d0cfba71b29facdcc4519a10c2cb555da7d
Ryan Voor <ryan.voor@gmail.com>
2017-11-14 13:13:14 -0500

    updated entity table alter with better naming

--- sql/alters/create_tb_entity.sql
+++ sql/alters/create_tb_entity.sql
@@ -1,7 +1,7 @@
-CREATE SEQUENCE sq_entity;
+CREATE SEQUENCE sq_pk_entity;
 
 CREATE TABLE tb_entity (
-    entity        integer primary key default nextval('sq_entity'),
+    entity        integer primary key default nextval('sq_pk_entity'),
     username      varchar unique not null,
     password      varchar not null,
     first_name    varchar not null,
