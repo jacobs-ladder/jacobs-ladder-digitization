3326e7052ebcbd396f5000c8bd84fb7ee6ebc5ca
Ryan Voor <ryan.voor@gmail.com>
2017-11-15 10:35:13 -0500

    add db connection function

--- db_lib.py
+++ db_lib.py
@@ -1,6 +1,30 @@
 import psycopg2
+import os
+import re
 
 #https://www.tutorialspoint.com/postgresql/postgresql_python.htm
+
+def get_db_connection():
+
+    # grab the DATABASE_URL config var
+    db_url = os.environ.get('DATABASE_URL')
+
+    # make sure that we got the DATABASE_URL
+    if db_url is None:
+        # TODO this should probably be a custom error type
+        raise IOError, "Could not get the DATABASE_URL config variable"
+
+    # parse the DATABASE_URL into the host, port, db_name, and db_user
+    # the url looks like this: postgres://db_username:db_host_name.com:port/name_of_db_itself
+    p = "^postgres://(.*):(.*):(\d*)/(.*)$"
+    db_user = re.match(p, db_url).group(1)
+    db_host = re.match(p, db_url).group(2)
+    db_port = re.match(p, db_url).group(3)
+    db_name = re.match(p, db_url).group(4)
+
+    # get the db_connection
+    return psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (db_name, db_user, db_host, db_port))
+
 
 #user_name, user_password
 def authenticate(username, password):
