02a1d2b72b6096d9db1610f782062087da23bcec
Ryan Voor <ryan.voor@gmail.com>
2017-11-17 16:11:34 -0500

    adjusted local db scripts so that they always show their output to help make errors more obvious

--- database_scripts/update_schema_local_safe.sh
+++ database_scripts/update_schema_local_safe.sh
@@ -17,43 +17,26 @@ psql -U $database_user -c "CREATE DATABASE $temp_database_name" &> /dev/null
 
 order_of_alters_full_file_name="sql/meta/order_of_alters"
 alter_file_path="sql/alters/"
-temp_database_alters_output_full_file_name="database_alters_output.txt"
-
-rm $temp_database_alters_output_full_file_name &> /dev/null
 
 while read line; do
     alter_full_file_name=$alter_file_path$line
     echo "APPLYING ALTER: $line"
-    psql -U $database_user -d $temp_database_name -f $alter_full_file_name -1 &> $temp_database_alters_output_full_file_name
+    psql -U $database_user -d $temp_database_name -f $alter_full_file_name -1
 done < $order_of_alters_full_file_name
 
-# check for errors when applying the alters
 
-if grep -q ERROR $temp_database_alters_output_full_file_name
-then
+# if no errors in temp's schema creation
 
-    # if there was an error while applying alters
-    echo "An error occurred while applying alters. Here is the output from applying alters:"
-    cat $temp_database_alters_output_full_file_name
-    rm $temp_database_alters_output_full_file_name &> /dev/null
+original_database_name="jacobs_ladder_digitization"
 
-else
-    # if no errors in temp's schema creation
+# drop original database
 
-    rm $temp_database_alters_output_full_file_name &> /dev/null
+psql -U $database_user -c "DROP DATABASE $original_database_name" &> /dev/null
 
-    original_database_name="jacobs_ladder_digitization"
+# rename temp to original name
 
-    # drop original database
+psql -U $database_user -c "ALTER DATABASE $temp_database_name RENAME TO $original_database_name" &> /dev/null
 
-    psql -U $database_user -c "DROP DATABASE $original_database_name" &> /dev/null
-
-    # rename temp to original name
-
-    psql -U $database_user -c "ALTER DATABASE $temp_database_name RENAME TO $original_database_name" &> /dev/null
-
-    # repopulate data
-    ./database_scripts/repopulate_data_local.sh
-
-fi
+# repopulate data
+./database_scripts/repopulate_data_local.sh
 
