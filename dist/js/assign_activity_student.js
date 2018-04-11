cf666630f12054344a2ef9fee26bc72a3d78fa01
Hyungsuk Do <grownhs@gmail.com>
2018-04-11 14:50:39 -0400

    assign_activity_creation.js temp

---
+++ dist/js/assign_activity_student.js
@@ -0,0 +1,223 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 53);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 53:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var UserInput = function (_React$Component) {
+	_inherits(UserInput, _React$Component);
+
+	function UserInput(props) {
+		_classCallCheck(this, UserInput);
+
+		var _this = _possibleConstructorReturn(this, (UserInput.__proto__ || Object.getPrototypeOf(UserInput)).call(this, props));
+
+		_this.state = {
+			username: "",
+			roles: "",
+			first_name: "",
+			last_name: "",
+			password: "",
+			email_address: ""
+		};
+		return _this;
+	}
+
+	_createClass(UserInput, [{
+		key: "formSubmit",
+		value: function formSubmit(event) {
+			$.post('../api/user', { username: this.state.username,
+				role_label: this.state.roles,
+				first_name: this.state.first_name,
+				last_name: this.state.last_name,
+				password: this.state.password,
+				email_address: this.state.email_address
+			}, function (returnedData) {
+				console.log(returnedData);
+			});
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			var _this2 = this;
+
+			return React.createElement(
+				"div",
+				null,
+				React.createElement(
+					"h2",
+					null,
+					"Create an User"
+				),
+				React.createElement(
+					"p",
+					null,
+					"User Name: ",
+					React.createElement("input", { type: "text", name: "username", value: this.state.username,
+						onChange: function onChange(evt) {
+							return _this2.setState({ username: evt.target.value });
+						} })
+				),
+				React.createElement(
+					"p",
+					null,
+					"Password: ",
+					React.createElement("input", { type: "text", name: "password", value: this.state.password,
+						onChange: function onChange(evt) {
+							return _this2.setState({ password: evt.target.value });
+						} })
+				),
+				React.createElement(
+					"p",
+					null,
+					"Email: ",
+					React.createElement("input", { type: "text", name: "email_address", value: this.state.email_address,
+						onChange: function onChange(evt) {
+							return _this2.setState({ email_address: evt.target.value });
+						} })
+				),
+				React.createElement(
+					"p",
+					null,
+					"Type of Roles: ",
+					React.createElement(
+						"select",
+						{ name: "role_label", value: this.state.roles,
+							onChange: function onChange(evt) {
+								return _this2.setState({ roles: evt.target.value });
+							} },
+						React.createElement(
+							"option",
+							{ value: "administrator" },
+							"Administrator"
+						),
+						React.createElement(
+							"option",
+							{ value: "evaluator" },
+							"Evaluator"
+						),
+						React.createElement(
+							"option",
+							{ value: "teacher" },
+							"Teacher"
+						)
+					)
+				),
+				React.createElement(
+					"p",
+					null,
+					"First Name: ",
+					React.createElement("input", { type: "text", name: "first_name", value: this.state.first_name,
+						onChange: function onChange(evt) {
+							return _this2.setState({ first_name: evt.target.value });
+						} })
+				),
+				React.createElement(
+					"p",
+					null,
+					"Last Name: ",
+					React.createElement("input", { type: "text", name: "last_name", value: this.state.last_name,
+						onChange: function onChange(evt) {
+							return _this2.setState({ last_name: evt.target.value });
+						} })
+				),
+				React.createElement("br", null),
+				React.createElement("br", null),
+				React.createElement(
+					"form",
+					{ action: "/userlist" },
+					React.createElement(
+						"p",
+						null,
+						React.createElement("input", { type: "submit", value: "Create an User", onClick: function onClick(evt) {
+								return _this2.formSubmit(evt);
+							} })
+					)
+				)
+			);
+		}
+	}]);
+
+	return UserInput;
+}(React.Component);
+
+ReactDOM.render(React.createElement(UserInput, null), document.getElementById('body'));
+
+/***/ })
+
+/******/ });
--- src/js/assign_activity_student.js
+++ src/js/assign_activity_student.js
@@ -1,255 +1,72 @@
-$.put = function(url, data, callback, type){
-
-  if ( $.isFunction(data) ){
-    type = type || callback,
-    callback = data,
-    data = {}
-  }
-
-  return $.ajax({
-    url: url,
-    type: 'PUT',
-    success: callback,
-    data: data,
-    contentType: type
-  });
-}
-
-
-class ColumnsFieldSet extends React.Component{
-	constructor(props){
-		super(props);
-
-		this.state = {
-		  	columns: [{ title: "", type:"numeric" }]
-		}
-
-		this.add = this.add.bind(this);
-	}
-
-	add() {
-		this.setState({ columns: this.state.columns.concat({ title: "", type:"numeric" }) });
-	}
-
-	remove(index) {
-		this.setState({ columns : this.state.columns.filter((s, sidx) => index !== sidx) });
-	}
-
-	titleChange(index, value) {
-		var columns = this.state.columns
-		columns[index].title = value
-		this.setState({ columns });
-	}
-
-	typeChange(index, value) {
-		var columns = this.state.columns
-		columns[index].type = value
-		this.setState({ columns });
-	}
-
-	getColumns(){
-		return this.state.columns.map((column,index) => {
-			return {
-				"title":column.title,
-		 		"number":index,
-		 		"data_type":column.type
-			}
-		});
-	}
-
-    componentDidMount(){
-		if(this.props.activity != -1){
-        	var self = this;
-			$.get( "/api/activity", {activity : this.props.activity}
-			, function( data ) {
-				var activity = JSON.parse(data);
-				self.setState({columns:activity.columns.map(col => { return {title:col[0], type:col[1]}; })});
-			});
-		}
-    }
-
-  	render () {
-    	const columns = this.state.columns.map((column, index) => {
-		  	return (
-				<div key={index}>
-					<br/>
-					Title: <input type="text" value={column.title}
-							onChange={(evt) => this.titleChange(index, evt.target.value)}/>
-					<span>  </span>
-					Type: <select name="input_type" value={column.type}
-							onChange={(evt) => this.typeChange(index, evt.target.value)}>
-			  			<option value="numeric">Numeric</option>
-			  			<option value="boolean">Checkbox</option>
-			  			<option value="string">Text</option>
-			  			<option value="timestamp">Time</option>
-					</select>
-					<span>  </span>
-					<button onClick={() => this.remove(index)}>Remove</button>
-				</div>
-			)
-    	});
-
-		return(
-			<div>
-				<p>Columns</p>
-		  		<button onClick={ this.add }>Add Column</button> <br/>
-		  		<div>{ columns }</div>
-			</div>
-		);
-  	}
-}
-class RowsFieldSet extends React.Component{
-	constructor(props){
-		super(props);
-
-		this.state = {
-		  	rows: [""]
-		}
-
-		this.add = this.add.bind(this);
-	}
-
-	add() {
-		this.setState({ rows: this.state.rows.concat("") });
-	}
-
-	remove(index) {
-		this.setState({ rows : this.state.rows.filter((s, sidx) => index !== sidx) });
-	}
-
-	titleChange(index, value) {
-		var rows = this.state.rows
-		rows[index] = value
-		this.setState({ rows });
-	}
-
-	getRows(){
-		return this.state.rows.map((row,index) => {
-			return {
-				"title":row,
-		 		"number":index,
-			}
-		});
-	}
-    componentDidMount(){
-		if(this.props.activity != -1){
-        	var self = this;
-			$.get( "/api/activity", {activity : this.props.activity}
-			, function( data ) {
-				var activity = JSON.parse(data);
-				self.setState({rows:activity.rows});
-			});
-		}
-    }
-
-  	render () {
-    	const rows = this.state.rows.map((row, index) => {
-		  	return (
-				<div key={index}>
-					<br/>
-					Title: <input type="text" value={row}
-							onChange={(evt) => this.titleChange(index, evt.target.value)}/>
-					<span>  </span>
-					<button onClick={() => this.remove(index)}>Remove</button>
-				</div>
-			)
-    	});
-
-		return(
-			<div>
-				<p>Rows</p>
-		  		<button onClick={ this.add }>Add Row</button> <br/>
-		  		<div>{ rows }</div>
-			</div>
-		);
-  	}
-}
-
-class ActivityInput extends React.Component{
+class assign_activity_student_Input extends React.Component{
 
 	constructor(props){
 		super(props)
 		this.state = {
-			title:"",
-			type:"numbers",
-			instructions:""
+			student_id:"",
+            activity_id:""
 		}
 	}
+    componentDidMount() {
+    	$.ajax({
+        	type: 'GET',
+            url: '../api/activity',
 
-    componentDidMount(){
-		if(this.props.activity != -1){
-        	var self = this;
-			$.get( "/api/activity", {activity : this.props.activity}
-			, function( data ) {
-				var activity = JSON.parse(data);
-				self.setState({ title : activity.title, type : activity.activity_type_label, instructions : activity.instructions });
-			});
-		}
-    }
+            dataType: "json",
+
+            success: function(data){
+    			render_activity(data);
+            },
+            error: function (request, status, error) {
+                alert(error);
+            }
+        });
+    });
 
 	formSubmit(event){
-		var columns_and_rows_json = JSON.stringify({
-			"columns": this.refs.columns.getColumns(),
-			"rows":this.refs.rows.getRows()
-		});
-		console.log(columns_and_rows_json);
-		$.post('../api/activity', { title:this.state.title,
-									activity_type:this.state.type,
-									instructions:this.state.instructions,
-									columns_and_rows : columns_and_rows_json},
+		$.post('/api/student_activity?student=' + String(data.id), {
+								student_id:this.state.student_id,
+                                activity_id:this.state.activity_id,
+								},
 			function(returnedData){
-                window.location.href = '/activitylist'
-		});
-	}
-
-	formSave(event){
-		var columns_and_rows_json = JSON.stringify({
-			"columns": this.refs.columns.getColumns(),
-			"rows":this.refs.rows.getRows()
-		});
-		console.log(columns_and_rows_json);
-		$.put('../api/activity', { activity:this.props.activity,
-                                    title:this.state.title,
-									activity_type:this.state.type,
-									instructions:this.state.instructions,
-									columns_and_rows : columns_and_rows_json},
-			function(returnedData){
-				console.log(returnedData);
-                window.location.href = '/activitylist'
+				 console.log(returnedData);
 		});
 	}
 
 	render() {
 		return (
 			<div>
-				<h2>{this.props.activity == -1 ? "Create" : "Edit"} an Activity</h2>
-			  		<p>Title: <input type="text" name="title" value={this.state.title}
-									onChange={(evt) => this.setState({ title:evt.target.value }) }/></p>
-			  		<p>Type of Activity: <select name="activity_type" value={this.state.type}
-									onChange={(evt) => this.setState({ type:evt.target.value }) }>
-				  		<option value="numbers">Numbers</option>
-				  		<option value="reading">Reading</option>
-				  		<option value="motor">Motor</option>
-				  		<option value="visual">Visual</option>
-					</select></p>
-			  	<p>Instructions: <textarea name="instructions"rows="10" cols="50" value={this.state.instructions}
-									onChange={(evt) => this.setState({ instructions:evt.target.value }) }></textarea></p>
-			  	<ColumnsFieldSet ref="columns" activity={this.props.activity}/>
+				<h2>Create a new Student</h2>
+			  	<p>First Name: <input type="text" name="first_name" value={this.state.first_name}
+                                onChange={(evt) => this.setState({ first_name:evt.target.value }) }/></p>
+                <p>Last Name: <input type="text" name="last_name" value={this.state.last_name}
+                                onChange={(evt) => this.setState({ last_name:evt.target.value }) }/></p>
 				<br/>
-			  	<RowsFieldSet ref="rows" activity={this.props.activity}/>
-				<br/>
-			    <p><input type="submit" value="Save Activity" onClick={(evt) => {
-															if(this.props.activity == -1){
-																this.formSubmit(evt);
-															} else {
-																this.formSave(evt);
-															}}}/></p>
+				<form action="/studentlist">
+				  <p><input type="submit" value="Create" onClick={(evt) => this.formSubmit(evt)}/></p>
+				</form>
+
 		  </div>
 	  );
 	}
+
+    function render_activity(data){
+        <p>Type of Roles: <select name="role_label" value={this.state.roles}
+                        onChange={(evt) => this.setState({ roles:evt.target.value }) }>
+            <option value="administrator">Administrator</option>
+            <option value="evaluator">Evaluator</option>
+            <option value="teacher">Teacher</option>
+        </select></p>
+    	const activity_list_table = <ReactTable data={data} defaultPageSize={10} columns={columns} filterable defaultFilterMethod= { (filter, row, column) => String(row[filter.id]).toLowerCase().startsWith(filter.value.toLowerCase())}/>
+
+    	ReactDOM.render(
+    	  	activity_list_table,
+    	 	document.getElementById('activity_list_table')
+    	);
+    }
 }
 
 ReactDOM.render(
-  <ActivityInput activity={activity_id}/>,
+  <StudentInput />,
   document.getElementById('body')
 );
